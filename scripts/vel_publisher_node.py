96be9759c01e331bc4ee15dd4c7fee1b7142fdae
kylie-g <kng1834@uncw.edu>
2023-01-24 20:16:34 +0000

    publisher code

---
+++ scripts/vel_publisher_node.py
@@ -0,0 +1,7 @@
+#environment address:
+#!/usr/bin/env python3
+
+#import ROS for developing the node
+import rospy
+#import geometry_msgs/Twist for control commands
+from geometry_msgs.msg import Twist
